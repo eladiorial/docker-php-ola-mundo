1ee80f651e8b6136e745b0bd0558986cffc1b5fe
eladiorial <eladio.rial@gmail.com>
2021-11-18 00:39:16 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,15 +5,15 @@
     <title>Docker PHP Ola Mundo</title>
   </head>
   <body>
-    <? if ($_POST['quen'] != '') {?>
+    <?php if ($_POST['quen'] != '') {?>
       <h1>Ola <?php echo htmlspecialchars($_POST['quen']); ?></h1>
       <a href="index.php">Saúda a alguén máis</a>
-    <? } else { ?>
+    <?php } else { ?>
       <form class="saudosForm" action="index.php" method="post">
         <label for="quen">Di ola a</label>
         <input type="text" name="quen">
         <input type="submit" name="saudo" value="Di ola">
       </form>
-    <? } ?>
+    <?php } ?>
   </body>
 </html>
